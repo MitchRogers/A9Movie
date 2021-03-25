746eb999d6a0e99af3f34a7afc94a1b685aaa27c
MitchRogers <mitchellmel@outlook.com>
2021-03-24 19:03:12 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using A9Movie.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A9Movie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDbContext context { get; set; }
+
+        public HomeController(MovieDbContext con)
+        {
+            context = con;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMovie(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Movies.Add(movie);
+                context.SaveChanges();
+            }
+            return View();
+        }
+
+        public IActionResult MovieList()
+        {
+            return View(context.Movies);
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
